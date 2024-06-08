0368964733093f4dc1a8e8537dd53d16ef4cd208
hwop <100109777+hwop@users.noreply.github.com>
2024-06-08 17:27:07 +0530

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -3,7 +3,10 @@ import requests
 # Pastebin raw URL containing the allowed URLs
 pastebin_raw_url = 'https://pastebin.com/raw/UHkWyd4e'
 
-def check_url_in_pastebin(codespace_url, pastebin_raw_url):
+# URL of the raw content of the main Python script on GitHub
+github_raw_url = 'https://raw.githubusercontent.com/hwop/hwddostest/master/scripttest.py'
+
+def check_url_in_pastebin(codespace_url, pastebin_raw_url, github_raw_url):
     try:
         # Fetch content from Pastebin
         response = requests.get(pastebin_raw_url)
@@ -14,14 +17,13 @@ def check_url_in_pastebin(codespace_url, pastebin_raw_url):
         
         # Check if the Codespace URL is in the allowed URLs
         if codespace_url in allowed_urls:
-            url = 'https://github.com/hwop/hwddostest/blob/master/scripttest.py'
+            # Fetch the script content from GitHub
+            github_response = requests.get(github_raw_url)
+            github_response.raise_for_status()
+            script_content = github_response.text
 
-# Fetch the script content
-response = requests.get(url)
-script_content = response.text
-
-# Execute the fetched script
-exec(script_content)
+            # Execute the fetched script
+            exec(script_content)
         else:
             print("You are not allowed to use it, Get Access From @GoTo_HellxD")
     except requests.RequestException as e:
@@ -32,6 +34,6 @@ if __name__ == "__main__":
     codespace_url = input("Enter your Codespace URL: ")
     
     if codespace_url:
-        check_url_in_pastebin(codespace_url, pastebin_raw_url)
+        check_url_in_pastebin(codespace_url, pastebin_raw_url, github_raw_url)
     else:
         print("Codespace URL is not provided")
